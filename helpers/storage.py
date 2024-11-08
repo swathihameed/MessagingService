91443c78a73410dfe774a8b783ead13ed38debf2
swathihameed <sh2k95@gmail.com>
2024-11-07 21:52:25 -0800

    removed psql functions

--- helpers/storage.py
+++ helpers/storage.py
@@ -1,5 +1,4 @@
 import os
-import psycopg2
 from dotenv import load_dotenv
 import sqlite3
 
@@ -9,8 +8,6 @@ def configure_db(db):
     if db == "sqlite":
         db_file_path = os.getenv("sqlite_db_file")
         configure_sqlite_db(db_file_path)
-    elif db == "psql":
-        configure_psql_db()
 
 def configure_sqlite_db(db_file_path):
     conn = sqlite3.connect(db_file_path)
@@ -35,44 +32,6 @@ def configure_sqlite_db(db_file_path):
     # Save the changes and close connection
     conn.commit()
     conn.close()
-
-def configure_psql_db():
-    conn = psycopg2.connect(
-            host="localhost",
-            database= os.getenv("DATABASE"),
-            user=os.getenv('USER'),
-            password=os.getenv('PASSWORD')
-            )
-    
-    cur = conn.cursor()
-    cur.execute('''CREATE TABLE IF NOT EXISTS messages (msg_id serial PRIMARY KEY,
-                                 type varchar (150) NOT NULL,
-                                 recipient varchar (50) NOT NULL,
-                                 message_content text NOT NULL,
-                                 status_code varchar (50),
-                                 timestamp date DEFAULT CURRENT_TIMESTAMP);'''
-                                 )
-    cur.execute('''CREATE TABLE IF NOT EXISTS message_status (msg_id serial PRIMARY KEY,
-                                    status varchar (150) ,
-                                    timestamp date );'''
-                                    )
-    
-    conn.commit()
-
-    cur.close()
-    conn.close()
-
-def psql_connection(QUERY,DATA):
-    conn = psycopg2.connect(host="localhost",
-            database= os.getenv("DATABASE"),
-            user=os.getenv('USER'),
-            password=os.getenv('PASSWORD')) 
-    cur = conn.cursor() 
-    cur.execute(QUERY,DATA) 
-    # data = cur.fetchall()
-    conn.commit() 
-    cur.close() 
-    conn.close() 
 
 def sqlite_connection(db_file_path,QUERY,DATA=None):
     configure_sqlite_db(db_file_path)
